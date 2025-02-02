c5b4f82c019c5ce1412cd20240f2369ba1453040
codehelixtest <rogern@nealit.com>
2025-02-02 11:57:15 -0800

    Apply patch for src/basic-collection.php

--- src/basic-collection.php
+++ src/basic-collection.php
@@ -1,16 +1,16 @@
 <?php
 
-// Cross-Site Scripting (XSS)
+$name = htmlspecialchars($_GET['name'], ENT_QUOTES, 'UTF-8'); echo('Hello ' . $name);
 $name = $_GET['name'];
-echo('Hello ' . $name);
-
-// SQL Injection
-$id = $_POST['id'];
-mysql_query("SELECT user FROM users WHERE id = " . $id);
-
-// Command Injection
+$name = htmlspecialchars($_GET['name'], ENT_QUOTES, 'UTF-8'); echo('Hello ' . $name);
+$id = intval($_POST['id']); mysql_query("SELECT user FROM users WHERE id = " . $id);
+$id = intval($_POST['id']); mysql_query('SELECT user FROM users WHERE id = ' . $id);
+$cmd = escapeshellarg($_COOKIE['cmd']); exec("cat /var/log/apache2/access.log | grep " . $cmd);
+$cmd = escapeshellarg($_COOKIE['cmd']); exec('cat /var/log/apache2/access.log | grep ' . $cmd);
+$words = explode(':', 'split:this');
+$words = explode(':', 'split:this');
 $cmd = $_COOKIE['cmd'];
-exec("cat /var/log/apache2/access.log | grep " . $cmd);
+$words = explode(':', 'split:this');
 
 // Deprecated Function
 $words = split(":", "split:this");
