90de35108acdb84055fdfd5ca5f3a3548b400f22
codehelixtest <rogern@nealit.com>
2025-02-02 13:26:39 -0800

    Apply patch for src/sql-injection.php

--- src/sql-injection.php
+++ src/sql-injection.php
@@ -4,13 +4,19 @@ if (PHP_SAPI === 'cli') {
     parse_str(implode('&', array_slice($argv, 1)), $_GET);
 }
 
-$file_db = new PDO('sqlite:../database/database.sqlite');
+$sql = 'SELECT * FROM employees WHERE employeeId = :id'; $stmt = $file_db->prepare($sql); $stmt->bindParam(':id', $_GET['id'], PDO::PARAM_INT); $stmt->execute();
 
 if (NULL == $_GET['id']) $_GET['id'] = 1;
 
 $sql = 'SELECT * FROM employees WHERE employeeId = ' . $_GET['id'];
 
-foreach ($file_db->query($sql) as $row) {
+$stmt = $file_db->prepare('SELECT * FROM employees WHERE employeeId = :id');
+$stmt->bindParam(':id', $_GET['id'], PDO::PARAM_INT);
+$stmt->execute();
+foreach ($stmt as $row) {
+    $employee = $row['LastName'] . " - " . $row['Email'] . "\n";
+    echo $employee;
+}
     $employee = $row['LastName'] . " - " . $row['Email'] . "\n";
 
     echo $employee;
